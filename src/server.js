b6e39aa729debf93b7e07d42771c0113727588bb
LuDiemert <lucianadiemert@gmail.com>
2024-02-17 18:23:22 -0300

    feat: add streams

--- src/server.js
+++ src/server.js
@@ -10,14 +10,31 @@ import http from "node:http";
 // - URL
 
 // GET, POST, PUT, PATCH, DELETE
-// GET => Fetch a resource from the back-end (Buscar um recurso)
+// GET => Fetch a resource from the back-end (Buscar um recurso = Fetch a resource)
 // POST => Create a resource in the back-end
 // PUT => Update a resource in the back-end 
 // PATCH => Update specific information about a resource in the back-end
 // DELETE => Delete a resource from the back-end
 
-// GET /users => Fetching users from the back-end(Buscando)
+// GET /users => Fetching users from the back-end(Fetching=Buscando)
 // POST /users => Create a user in the back-end
+
+/*Stateful - Stateless 
+o stateless é um widget estático, ou seja, não temos como gerenciar o 
+estado dele. E o stateful é completamente dinâmico e nos dá o poder de 
+torná-lo mutável através da gerência de estados*/
+
+// headers (requests / response) => Metadata
+
+/*HTTP status code
+Informational responses (100 – 199)
+Successful responses (200 – 299)
+Redirection messages (300 – 399)
+Client error responses (400 – 499)
+Server error responses (500 – 599)
+
+*/
+
 
 const users = []
 
@@ -25,7 +42,9 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('users')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
@@ -34,10 +53,10 @@ const server = http.createServer((req, res) => {
       name: 'John Dane',
       email: 'johndane@gmail.com',
     })
-    return res.end('Criacao de usuarios')
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end('Not Found!!!')
 
 });
 
